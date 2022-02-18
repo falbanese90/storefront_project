d7be4b63b705aa9a21e9ecf7e209839c642fcf1e
Frank Albanese <albanesefc9@gmail.com>
2022-02-18 07:15:16 -0500

    Third Submission

--- dist/tests/serverSpec.js
+++ dist/tests/serverSpec.js
@@ -4,71 +4,109 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("../server"));
-describe('All endpoints return status 200', () => {
-    it('Connection to "/" is status 200', () => {
-        server_1.default.get('/', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(200);
-        });
+const supertest_1 = __importDefault(require("supertest"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const database_1 = __importDefault(require("../database"));
+dotenv_1.default.config();
+const request = (0, supertest_1.default)(server_1.default);
+const user = {
+    firstName: "Frank",
+    lastName: "Albanese",
+    password: "password123"
+};
+const product = {
+    name: "Product_1",
+    price: 200
+};
+const order = {
+    status: "active",
+    user_id: user.id
+};
+const addProduct = {
+    quantity: 10,
+    productId: String(product.id)
+};
+const token = jsonwebtoken_1.default.sign(user, process.env.TOKEN_SECRET);
+describe('Tests all server endpoints', () => {
+    it('Users index should return status 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .get('/users')
+            .set('Authorization', `Bearer ${token}`);
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/products" is status 200', () => {
-        server_1.default.get('/products', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(200);
-        });
+    it('Create users returns status 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .post('/users/create')
+            .send({ user });
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/products" is status 401 - unauthorized', () => {
-        server_1.default.post('/products/create', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
+    it('Show specified User returns status 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .get(`/users/show/${user.id}`)
+            .set('Authorization', `Bearer ${token}`);
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/products/show/8" is status 404 - Not Found', () => {
-        server_1.default.get('/products/show/8', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(404);
-        });
+    it('Index products returns 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .get('/products');
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/users" is status 401 - unathorized', () => {
-        server_1.default.get('/users', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
+    it('Index products returns 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .get('/products');
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/users/create" is status 200', () => {
-        server_1.default.post('/users/create', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(200);
-        });
+    it('Create product endpoint returns 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .post('/products/create')
+            .set('Authorization', `Bearer ${token}`)
+            .send({ product });
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/users/authenticate" is status 401', () => {
-        server_1.default.post('/users/authenticate', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
+    it('Create product endpoint returns 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .get(`/products/show/${product.id}`)
+            .set('Authorization', `Bearer ${token}`);
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/orders" is status 200', () => {
-        server_1.default.get('/orders', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(200);
-        });
+    it('Index orders endpoint returns 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .get(`/orders`)
+            .set('Authorization', `Bearer ${token}`);
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/orders/create" is status 401', () => {
-        server_1.default.post('/orders/create', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
+    it('Create orders endpoint returns 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .post(`/orders/create`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({ order });
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/orders/id/10" is status 401', () => {
-        server_1.default.get('/orders/id/10', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
+    it('Show specific order endpoint returns 200', async () => {
+        const response = await (0, supertest_1.default)(server_1.default)
+            .get(`/orders/${order.id}`)
+            .set('Authorization', `Bearer ${token}`);
+        expect(response.statusCode).toBe(200);
     });
-    it('Connection to "/orders/id/10/products" is status 401', () => {
-        server_1.default.post('/orders/id/10/products', (req, res) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
+    it('Add product to order endpoint returns 200', async () => {
+        try {
+            const response = await (0, supertest_1.default)(server_1.default)
+                .post(`/orders/${order.id}/products`)
+                .set('Authorization', `Bearer ${token}`)
+                .send({ addProduct });
+            expect(response.statusCode).toBe(200);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+    afterAll(async () => {
+        const conn = await database_1.default.connect();
+        await conn.query('DELETE FROM order_products_table;');
+        await conn.query('DELETE FROM orders;');
+        await conn.query('DELETE FROM products;');
+        await conn.query('DELETE FROM users;');
+        conn.release();
     });
 });
--- src/tests/serverSpec.ts
+++ src/tests/serverSpec.ts
@@ -1,74 +1,114 @@
 import app from '../server';
 import { Request, Response } from 'express';
+import { User } from '../models/users';
+import supertest from 'supertest';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { Product } from '../models/products';
+import { Order } from '../models/orders';
+import Client from '../database';
 
-describe('All endpoints return status 200', () => {
-    it('Connection to "/" is status 200', () => {
-        app.get('/', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(200);
-        });
-    });
+dotenv.config();
 
-    it('Connection to "/products" is status 200', () => {
-        app.get('/products', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(200);
-        });
-    });
-    it('Connection to "/products" is status 401 - unauthorized', () => {
-        app.post('/products/create', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
-    });
-    it('Connection to "/products/show/8" is status 404 - Not Found', () => {
-        app.get('/products/show/8', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(404);
-        });
-    });
+const request = supertest(app);
+const user: User = {
+    firstName: "Frank",
+    lastName: "Albanese",
+    password: "password123"
+}
+const product: Product = {
+    name: "Product_1",
+    price: 200
+}
+const order: Order = {
+    status: "active",
+    user_id: user.id as number
+}
+const addProduct: Object = {
+    quantity: 10,
+    productId: String(product.id)
+}
 
-    it('Connection to "/users" is status 401 - unathorized', () => {
-        app.get('/users', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
-    });
-    it('Connection to "/users/create" is status 200', () => {
-        app.post('/users/create', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(200);
-        });
-    });
-    it('Connection to "/users/authenticate" is status 401', () => {
-        app.post('/users/authenticate', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
-    });
+const token = jwt.sign(user , process.env.TOKEN_SECRET as string);
 
-    it('Connection to "/orders" is status 200', () => {
-        app.get('/orders', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(200);
-        });
-    });
-    it('Connection to "/orders/create" is status 401', () => {
-        app.post('/orders/create', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
-    });
-    it('Connection to "/orders/id/10" is status 401', () => {
-        app.get('/orders/id/10', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
-    });
-    it('Connection to "/orders/id/10/products" is status 401', () => {
-        app.post('/orders/id/10/products', (req: Request, res: Response) => {
-            const code = res.status;
-            expect(code).toBe(401);
-        });
+describe('Tests all server endpoints', () => {
+    it('Users index should return status 200', async () => {
+        const response = await supertest(app)
+            .get('/users')
+            .set('Authorization', `Bearer ${token}`)
+        expect(response.statusCode).toBe(200);
+    })
+    it('Create users returns status 200', async () => {
+        const response = await supertest(app)
+            .post('/users/create')
+            .send({user});
+        expect(response.statusCode).toBe(200);
+    })
+    it('Show specified User returns status 200', async () => {
+        const response = await supertest(app)
+            .get(`/users/show/${user.id}`)
+            .set('Authorization', `Bearer ${token}`)
+        expect(response.statusCode).toBe(200);
+    })
+    it('Index products returns 200', async () => {
+        const response = await supertest(app)
+            .get('/products')
+        expect(response.statusCode).toBe(200);
+    })
+    it('Index products returns 200', async () => {
+        const response = await supertest(app)
+            .get('/products')
+        expect(response.statusCode).toBe(200);
+    })
+    it('Create product endpoint returns 200', async () => {
+        const response = await supertest(app)
+            .post('/products/create')
+            .set('Authorization', `Bearer ${token}`)
+            .send({product})
+        expect(response.statusCode).toBe(200);
+    })
+    it('Create product endpoint returns 200', async () => {
+        const response = await supertest(app)
+            .get(`/products/show/${product.id}`)
+            .set('Authorization', `Bearer ${token}`)
+        expect(response.statusCode).toBe(200);
+    })
+    it('Index orders endpoint returns 200', async () => {
+        const response = await supertest(app)
+            .get(`/orders`)
+            .set('Authorization', `Bearer ${token}`)
+        expect(response.statusCode).toBe(200);
+    })
+    it('Create orders endpoint returns 200', async () => {
+        const response = await supertest(app)
+            .post(`/orders/create`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({order})
+        expect(response.statusCode).toBe(200);
+    })
+    it('Show specific order endpoint returns 200', async () => {
+        const response = await supertest(app)
+            .get(`/orders/${order.id}`)
+            .set('Authorization', `Bearer ${token}`)
+        expect(response.statusCode).toBe(200);
+    })
+    it('Add product to order endpoint returns 200', async () => {
+        try {
+            const response = await supertest(app)
+                .post(`/orders/${order.id}/products`)
+                .set('Authorization', `Bearer ${token}`)
+                .send({addProduct})
+         expect(response.statusCode).toBe(200);
+        } catch (err) {
+            console.log(err)
+        }
+    })
+    afterAll(async () => {
+        const conn = await Client.connect();
+        await conn.query('DELETE FROM order_products_table;');
+        await conn.query('DELETE FROM orders;');
+        await conn.query('DELETE FROM products;');
+        await conn.query('DELETE FROM users;');
+        conn.release();
     });
 });
